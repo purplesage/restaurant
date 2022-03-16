cb9e9f0e073a5ede932d92f209f8d89d72afa4df
David Mendoza <hectormza21@gmail.com>
2022-03-16 13:47:18 -0400

    mock content done.

--- src/contact.js
+++ src/contact.js
@@ -1,10 +1,21 @@
+import theMap from './assets/images/ashe.png'; //this will be changed.
 const contactContent = () => {
 
     let contentDiv = document.createElement('div');
-    let mock = document.createElement('p');
-    mock.textContent = "mock content";
+    
+    let telephone = document.createElement('p');
+    telephone.textContent = "(0212) 271 70 85";
 
-    contentDiv.appendChild(mock);
+    let adress = document.createElement('p');
+    adress.textContent = "some random adress";
+
+    let map = new Image();
+    map.src = theMap;
+
+    contentDiv.appendChild(telephone);
+    contentDiv.appendChild(adress);
+    contentDiv.appendChild(map);
+
     contentDiv.classList = 'contact-content';
     contentDiv.setAttribute('id', 'content');
     
